d3c350949a4e50cba20cca12d260c35c588a274b
machinesandpixels <>
2022-09-24 00:52:32 +0200

    feat(Card): added favorite heart icon to Card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import Image from 'next/image';
+import { FiHeart } from 'react-icons/fi';
+
 import styles from '../../styles/ImageGallery.module.css';
 
 const Card = () => (
   <div className={styles.card}>
     {/* <span className={styles.logo}> */}
     <div className={`${styles.imageContainer}`}>
+      <div className={styles.favoriteIcon}>
+        <FiHeart
+          fill="grey"
+          size="15"
+        />
+      </div>
       <Image
         src="https://picsum.photos/200/?grayscale"
         alt="card-image"

21f8f724d2f5d90506242b1d6a7faa7d2e2160d0
machinesandpixels <>
2022-09-24 00:21:47 +0200

    refactor(Card): updated css classname from cardSm to imageContainer

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,13 +5,15 @@ import styles from '../../styles/ImageGallery.module.css';
 const Card = () => (
   <div className={styles.card}>
     {/* <span className={styles.logo}> */}
-    <Image
-      src="https://picsum.photos/200/?grayscale"
-      alt="card-image"
-      width={200}
-      height={200}
-    />
-    {/* </span> */}
+    <div className={`${styles.imageContainer}`}>
+      <Image
+        src="https://picsum.photos/200/?grayscale"
+        alt="card-image"
+        width={200}
+        height={200}
+      />
+      {/* </span> */}
+    </div>
   </div>
 );
 

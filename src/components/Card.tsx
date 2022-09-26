6c73af1d5ab56fd50e8294a482af7dfd199ee7c1
machinesandpixels <>
2022-09-25 20:57:57 -0700

    feat(Card): added a few style changes to Card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import Image from 'next/image';
 import { FiHeart } from 'react-icons/fi';
-
 import styles from '../../styles/ImageGallery.module.css';
 
 const Card = () => {
@@ -14,22 +13,25 @@ const Card = () => {
   return (
     <div className={styles.card}>
       {/* <span className={styles.logo}> */}
-      <div className={`${styles.imageContainer}`}>
+      {/* <div className={`${styles.cardContainer}`}> */}
+      <div className={styles.imageContainer}>
         <div className={styles.favoriteIcon}>
           <FiHeart
             onClick={handleFavorite}
-            fill={isFavorite ? 'grey' : ''}
-            size="15"
+            fill={isFavorite ? 'grey' : 'white'}
+            size="11"
           />
         </div>
         <Image
-          src="https://picsum.photos/200/?grayscale"
+          className={styles.image}
+          src="https://picsum.photos/200/"
           alt="card-image"
           width={200}
           height={200}
         />
-        {/* </span> */}
       </div>
+      {/* </span> */}
+      {/* </div> */}
     </div>
   );
 };

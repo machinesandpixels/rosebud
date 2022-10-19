a5133cca87c76cfdcd47532465f742b45bcae9b6
Machines & Pixels <quezada.dev@gmail.com>
2022-10-19 14:09:25 -0700

    feat(Header, Footer): created Header and Footer components

--- src/pages/images.tsx
+++ src/pages/images.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import type { NextPage } from 'next';
+import Header from '../components/Header';
 import TrendingColumn from '../components/TrendingColumn';
 import FavoritesColumn from '../components/FavoritesColumn';
 import ImageGalleryColumn from '../components/ImageGalleryColumn';
+import Footer from '../components/Footer';
 import styles from '../../styles/ImageGallery.module.css';
 
 const Images: NextPage = () => (
+  <div>
+  <Header />
   <main className={styles.grid}>
     <TrendingColumn />
     <ImageGalleryColumn />
     <FavoritesColumn />
   </main>
+  <Footer />
+  </div>
 );
 
 export default Images;

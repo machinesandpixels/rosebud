ac5423da4ded75fb2eaf60873198304b70f2b582
Machines & Pixels <quezada.dev@gmail.com>
2022-10-28 02:55:09 -0500

    feat: implemented darkmode and updated Header text

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import {
+  Button,
+  useColorMode,
+} from '@chakra-ui/react';
 import styles from '../../styles/ImageGallery.module.css';
 
-const Header = () => (
-  <div>
-    <h1 className={styles.header}>Rosebud</h1>
-  </div>
-);
+const Header = () => {
+  const { toggleColorMode } = useColorMode();
+  return (
+    <div className={styles.headerContainer}>
+      <h1 className={styles.header}>Rosebud&apos;s Gallery</h1>
+      {/* <Button onClick={toggleColorMode} variant="link" colorScheme="black">
+        try changing the theme.
+      </Button> */}
+    </div>
+  );
+};
 
 export default Header;
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,9 +1,27 @@
 import { extendTheme, theme as base } from '@chakra-ui/react';
+import { mode } from '@chakra-ui/theme-tools';
 
 const theme = extendTheme({
   fonts: {
     heading: `Readex Pro, ${base.fonts?.heading}`,
     body: `Readex Pro, ${base.fonts?.body}`,
+  },
+  styles: {
+    global: (props) => ({
+      body: {
+        fontFamily: 'body',
+        color: mode('gray.800', 'whiteAlpha.900')(props),
+        bg: mode('white', 'black')(props),
+        lineHeight: 'base',
+      },
+      '*::placeholder': {
+        color: mode('gray.400', 'whiteAlpha.400')(props),
+      },
+      '*, *::before, &::after': {
+        borderColor: mode('gray.200', 'whiteAlpha.300')(props),
+        wordWrap: 'break-word',
+      },
+    }),
   },
 });
 

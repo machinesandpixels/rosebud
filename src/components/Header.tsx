4298b2611730738e6fedb671a9c88c82f4603db1
Machines & Pixels <quezada.dev@gmail.com>
2022-10-28 11:00:22 -0500

    feat(Header): added icon to toggle colormodes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { AiOutlineCamera } from 'react-icons/ai';
 import {
   Button,
   useColorMode,
@@ -10,10 +11,17 @@ const Header = () => {
   return (
     <div className={styles.headerContainer}>
       <h1 className={styles.header}>Rosebud&apos;s Gallery</h1>
+      {/* <h1 className={styles.header}>Rosebud Images</h1> */}
       <p>Find Your Inner Cinematographer</p>
       {/* <Button onClick={toggleColorMode} variant="link" colorScheme="black">
         try changing the theme.
       </Button> */}
+      <div className={styles.colorModeContainer}>
+        <AiOutlineCamera
+          onClick={toggleColorMode}
+          size="25"
+        />
+      </div>
     </div>
   );
 };
